0a54b90a57cbdc4118f4caf6b0406d9a522c4f2b
PedroIvan1970 <pquintana@elsoldemexico.com.mx>
2025-02-26 10:53:13 -0600

    Cambiando el puerto de Flask a 8080

--- servidor.py
+++ servidor.py
@@ -28,4 +28,4 @@ def obtener_tipo_cambio():
         return jsonify({"error": "No se pudo obtener el tipo de cambio"}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
